7ac6c01d90e94607824b370dedc9b46915dac52c
arghpppd <arghpppd@gmail.com>
2010-11-30 23:57:42 +0200

    doma6no stoqn

---
+++ src/main/java/jia/begins/exercises/basics/stoyan/panayotov/Sum1000.java
@@ -0,0 +1,22 @@
+package jia.begins.exercises.basics.stoyan.panayotov;
+
+/**
+ * Created by IntelliJ IDEA.
+ * User: Stoqn
+ * Date: Nov 30, 2010
+ * Time: 11:48:58 PM
+ * To change this template use File | Settings | File Templates.
+ */
+
+ public class Sum1000 {
+
+
+     public static void main(String[] args) {
+         int sum=0;
+         for(int i=1;i<=1000;i++){
+             sum+=i;
+         }
+         System.out.println("Sum of numbers = "+sum);
+     }
+
+ }
